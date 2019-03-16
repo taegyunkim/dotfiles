41f458e59f3765dacc3e59a1a7c754ea3413bb0d
Taegyun Kim <k.taegyun@gmail.com>
2019-03-16 12:50:17 -0400

    Remove ycm specific parts.

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -49,42 +49,12 @@ flags = [
     '-Wno-variadic-macros',
     '-fexceptions',
     '-DNDEBUG',
-    # You 100% do NOT need -DUSE_CLANG_COMPLETER and/or -DYCM_EXPORT in your flags;
-    # only the YCM source code needs it.
-    '-DUSE_CLANG_COMPLETER',
-    '-DYCM_EXPORT=',
     # THIS IS IMPORTANT! Without the '-x' flag, Clang won't know which language to
     # use when compiling headers. So it will guess. Badly. So C++ headers will be
     # compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
     # For a C project, you would set this to 'c' instead of 'c++'.
     '-x',
     'c++',
-    '-isystem',
-    'cpp/pybind11',
-    '-isystem',
-    'cpp/whereami',
-    '-isystem',
-    'cpp/BoostParts',
-    '-isystem',
-    get_python_inc(),
-    '-isystem',
-    'cpp/llvm/include',
-    '-isystem',
-    'cpp/llvm/tools/clang/include',
-    '-I',
-    'cpp/ycm',
-    '-I',
-    'cpp/ycm/ClangCompleter',
-    '-isystem',
-    'cpp/ycm/tests/gmock/gtest',
-    '-isystem',
-    'cpp/ycm/tests/gmock/gtest/include',
-    '-isystem',
-    'cpp/ycm/tests/gmock',
-    '-isystem',
-    'cpp/ycm/tests/gmock/include',
-    '-isystem',
-    'cpp/ycm/benchmarks/benchmark/include',
 ]
 
 # Clang automatically sets the '-std=' flag to 'c++14' for MSVC 2015 or later,
