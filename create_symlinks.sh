ee7dbb5f94a782f3e53e72851717db4c5119a4f8
Taegyun Kim <k.taegyun@gmail.com>
2022-04-30 18:25:35 -0400

    remove prezto related scripts

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -10,3 +10,8 @@ ln -nfs ~/.dotfiles/tmux ~/.tmux
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/ycm_extra_conf.py ~/.ycm_extra_conf.py
 ln -s ~/.dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+ln -s ~/.dotfiles/zsh/zlogin ~/.zlogin
+ln -s ~/.dotfiles/zsh/zlogout ~/.zlogout
+ln -s ~/.dotfiles/zsh/zprofile ~/.zprofile
+ln -s ~/.dotfiles/zsh/zshenv ~/.zshenv
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
