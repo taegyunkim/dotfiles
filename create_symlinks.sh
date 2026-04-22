7b0111f651c88014b7751e13296d6c5165956e9a
Taegyun Kim <k.taegyun@gmail.com>
2026-04-22 19:54:33 +0900

    zsh: fix login shell path setup

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -8,11 +8,11 @@ ln -nfs ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/.dotfiles/tmux ~/.tmux
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/zsh/p10k.zsh ~/.p10k.zsh
-ln -s ~/.dotfiles/zsh/zlogin ~/.zlogin
-ln -s ~/.dotfiles/zsh/zprofile ~/.zprofile
-ln -s ~/.dotfiles/zsh/zshenv ~/.zshenv
-ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -nfs ~/.dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+ln -nfs ~/.dotfiles/zsh/zlogin ~/.zlogin
+ln -nfs ~/.dotfiles/zsh/zprofile ~/.zprofile
+ln -nfs ~/.dotfiles/zsh/zshenv ~/.zshenv
+ln -nfs ~/.dotfiles/zsh/zshrc ~/.zshrc
 mkdir -p ~/.config/nvim && ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 mkdir -p ~/.local/share && ln -nfs ~/.dotfiles/vim ~/.local/share/nvim
 ~/.dotfiles/claude/install.sh
